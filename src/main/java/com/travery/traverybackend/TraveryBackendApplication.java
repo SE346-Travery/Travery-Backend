d5fcda45fde58c4f7cd9964ed620b16b6a8cc78c
ducchinh <hoangducchinh730@gmail.com>
2026-03-23 15:24:57 +0700

    Stuff

---
+++ src/main/java/com/travery/traverybackend/TraveryBackendApplication.java
@@ -0,0 +1,12 @@
+package com.travery.traverybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveryBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TraveryBackendApplication.class, args);
+  }
+}
---
+++ src/test/java/com/travery/traverybackend/TraveryBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.travery.traverybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveryBackendApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
