ed8330acbcc9e537fc2afaee0dc985b70193b60d
LeonLH928 <lehuynhanhtuan123@gmail.com>
2026-05-01 12:43:21 +0700

    chore: add @Valid to members in CreateBookingRequest

--- src/main/java/com/travery/traverybackend/dtos/request/booking/CreateBookingRequest.java
+++ src/main/java/com/travery/traverybackend/dtos/request/booking/CreateBookingRequest.java
@@ -1,5 +1,6 @@
 package com.travery.traverybackend.dtos.request.booking;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -33,5 +34,6 @@ public class CreateBookingRequest {
 
   private String specialNotes;
 
+  @Valid
   private List<BookingMemberRequest> members;
 }
