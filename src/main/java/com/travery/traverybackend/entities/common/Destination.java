2a9b89162d98f570d722ff5315823944de53a55c
ducchinh <hoangducchinh730@gmail.com>
2026-05-29 17:25:55 +0700

    feat(common): simplify destination entity and link to station

--- src/main/java/com/travery/traverybackend/entities/common/Destination.java
+++ src/main/java/com/travery/traverybackend/entities/common/Destination.java
@@ -2,8 +2,8 @@ package com.travery.traverybackend.entities.common;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.travery.traverybackend.entities.AbstractBaseEntity;
+import com.travery.traverybackend.entities.coach.Station;
 import com.travery.traverybackend.entities.tour.Tour;
-import com.travery.traverybackend.enums.common.Region;
 import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 
 @Entity
 @Table(name = "destinations")
@@ -21,6 +22,7 @@ import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextFi
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
+@Indexed
 public class Destination extends AbstractBaseEntity {
 
   @Column(nullable = false, unique = true, length = 50)
@@ -30,17 +32,11 @@ public class Destination extends AbstractBaseEntity {
   @Column(nullable = false, length = 255)
   private String name;
 
-  @Enumerated(EnumType.STRING)
-  @Column(nullable = false, length = 20)
-  private Region region;
-
-  @Column(name = "image_url", length = 500)
-  private String imageUrl;
-
-  @Column(columnDefinition = "TEXT")
-  private String description;
-
   @JsonIgnore
   @OneToMany(mappedBy = "destination")
   private List<Tour> tours = new ArrayList<>();
+
+  @JsonIgnore
+  @OneToMany(mappedBy = "destination", cascade = CascadeType.ALL)
+  private List<Station> stations = new ArrayList<>();
 }
